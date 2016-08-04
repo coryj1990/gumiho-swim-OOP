7b22968a3b85b6f45c6e2c2f8ceef46e4514f2e3
Cory Johnson <cory.r.johnson@cox.net>
2016-08-04 09:48:06 -0500

    Finished with find/update functions for ATHLETES table. Onto EVENTS and COMPETEINFO

--- app/models/entryformsql.rb
+++ app/models/entryformsql.rb
@@ -2,10 +2,20 @@ require 'pry'
 require_relative '../controllers/secretstuff.rb'
 require 'HTTParty'
 
+# This file contains all SQL class commands to manage ATHLETE and some COMPETEINFO records
+# in the database
 
 class EntryformSQL
-
+	# attr_accessor is a combo of the reader and writer attr stuffs.
 	attr_accessor :name, :college, :address, :eventstring, :eventsarr, :conf, :id
+
+	# Copied functions from entryform models to get functions within class to work
+	# These latget and eastorwest functions are all that are needed to determine
+	# conference of athlete given their address of university.
+
+	# latget takes an address. Can be full address or just city, state format
+	#
+	# returns latitude coordinate from google api map
 
 	def latget(spot)
 		hasher = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{spot}&key=#{API_KEY}")
@@ -14,6 +24,9 @@ class EntryformSQL
 		return tochecklat
 	end
 
+	# Takes an address, puts it into latget, and compares it to the latitude coordinate of the mississippi
+	# 
+	# Returns Eastern or Western if the address's coordinate is East or West of the mississippi
 
 	def eastorwest(theaddr)
 		if latget(theaddr) > -95.2075
@@ -25,6 +38,15 @@ class EntryformSQL
 
 		return eorwarray
 	end
+
+	# At this state of our product, the event coordinator is entering an athlete's name, address, college,
+	# as well as checking off all the events the athlete is in. *WIP: because this is testing, no
+	# integration has been made to take the params passed on from the HTML forms to generate event string,
+	# but there will be*
+	#
+	# Initialize will take all info and store them into ATHLETES and COMPETEINFO TABLE.
+	#
+	# returns nothing!
 
 	def initialize (name, college, address, eventstring)
 		@name = name
@@ -44,6 +66,62 @@ class EntryformSQL
 		end
 
 	end
+
+	# Returns record from ATHLETES based on ID
+	def self.find_by_id(id)
+
+		return DB.execute("SELECT * FROM ATHLETES WHERE ATHLETEID = \"#{id}\"")
+
+	end
+
+	# Returns record from ATHLETES based on name
+	def self.find_by_name(name)
+
+		return DB.execute("SELECT * FROM ATHLETES WHERE NAME = \"#{name}\"")
+
+	end
+
+	# If a record needs to be updated (either just a single column, or the entire thing) supply this 
+	# function with the ID, name, college, address, and events of the athlete.
+	#
+	# returns object with updated record info
+	def self.update_record_by_id(id, name, college, address, eventstring)
+
+		DB.execute("UPDATE ATHLETES SET NAME = \"#{name}\" WHERE ATHLETEID = #{id}")
+		DB.execute("UPDATE ATHLETES SET COLLEGE = \"#{college}\" WHERE ATHLETEID = #{id}")
+		DB.execute("UPDATE ATHLETES SET ADDRESS = \"#{address}\" WHERE ATHLETEID = #{id}")
+		@newconf = eastorwest(address)
+		DB.execute("UPDATE ATHLETES SET CONF = \"#{@newconf}\" WHERE ATHLETEID = #{id}")
+		@neweventsarr = eventstring.split(", ")
+		n = 0
+		while n < @neweventsarr.size
+			DB.execute("INSERT INTO COMPETEINFO (EVENTID, ATHLETEID) VALUES ((SELECT EVENTABV FROM EVENTS WHERE EVENTNAME = \"#{@neweventsarr[n]}\"), (SELECT ATHLETEID FROM ATHLETES WHERE NAME = \"#{name}\"))")
+			n += 1
+		end
+
+		return DB.execute("SELECT * FROM ATHLETES WHERE ATHLETEID = #{id}")
+
+	end
+
+	# Same principle as update_record_by_id, this update function is based on the athlete's name 
+	#
+	# returns updated record object.
+	def self.update_record_by_name(name, college, address, eventstring)
+
+		DB.execute("UPDATE ATHLETES SET COLLEGE = \"#{college}\" WHERE NAME = \"#{name}\"")
+		DB.execute("UPDATE ATHLETES SET ADDRESS = \"#{address}\" WHERE NAME = \"#{name}\"")
+		@newconf = eastorwest(address)
+		DB.execute("UPDATE ATHLETES SET CONF = \"#{@newconf}\" WHERE NAME = \"#{name}\"")
+		@neweventsarr = eventstring.split(", ")
+		n = 0
+		while n < @neweventsarr.size
+			DB.execute("INSERT INTO COMPETEINFO (EVENTID, ATHLETEID) VALUES ((SELECT EVENTABV FROM EVENTS WHERE EVENTNAME = \"#{@neweventsarr[n]}\"), (SELECT ATHLETEID FROM ATHLETES WHERE NAME = \"#{name}\"))")
+			n += 1
+		end
+
+		return DB.execute("SELECT * FROM ATHLETES WHERE NAME = \"#{name}\"")
+
+	end	
 
 
 
--- tests/entryformsql_test.rb
+++ tests/entryformsql_test.rb
@@ -5,7 +5,18 @@ class EntryformSQLTest < Minitest::Test
 
 	def setup
 		super
-
+		# initially two athletes will be put into test.db
+		#
+		# The below executions makes sure there are no present data inside any tables.
+		# The tables we are working with are ATHLETES, EVENTS, and COMPETEINFO
+		# ATHLETES is compsed of ATHLETEID, NAME, ADDRESS, COLLEGE, CONF
+		#
+		# EVENTS is a static table (only needed to make it once) and serves the purpose of matching
+		# EVENTABV to the EVENTNAME
+		#
+		# COMPETEINFO IS MADE UP OF ROWID, ATHLETEID, EVENTID, AND TIMES. ATHLETE ID and EVENTID
+		# can already be put into COMPETEINFO. TIMES will be updated later when our so called
+		# swimmeet will start (because at this stage we are just entering athletes, meet hasn't begun).
 		DB.execute("DROP TABLE IF EXISTS ATHLETES")
 		DB.execute("DROP TABLE IF EXISTS COMPETEINFO")
 		DB.execute("CREATE TABLE IF NOT EXISTS ATHLETES (ATHLETEID INTEGER PRIMARY KEY, COLLEGE TEXT, CONF TEXT, ADDRESS TEXT, NAME TEXT)")
@@ -17,20 +28,22 @@ class EntryformSQLTest < Minitest::Test
 	end
 
 	def test_object_created
-
+		# Just to see if it truely is an object
 		assert_kind_of Object, @objname
 	end
 
 	def test_athlete_table_if_entered
-
-		assert_equal DB.execute("SELECT NAME FROM ATHLETES WHERE ATHLETEID = \"#{@objname.id}\"")[0]['NAME'], @objname.name
-		assert_equal DB.execute("SELECT ADDRESS FROM ATHLETES WHERE ATHLETEID = \"#{@objname.id}\"")[0]['ADDRESS'], @objname.address
-		assert_equal DB.execute("SELECT COLLEGE FROM ATHLETES WHERE ATHLETEID = \"#{@objname.id}\"")[0]['COLLEGE'], @objname.college
+		# Just to see if entered data matches find_by_id function
+		assert_equal EntryformSQL.find_by_id(@objname.id)[0]['NAME'], @objname.name
+		assert_equal EntryformSQL.find_by_id(@objname.id)[0]['ADDRESS'], @objname.address
+		assert_equal EntryformSQL.find_by_id(@objname.id)[0]['COLLEGE'], @objname.college
 
 	end
 
 	def test_competeinfo_table_if_entered_matches
-
+		# have yet to start on competeinfosql.rb or eventssql.rb to make ORM functions, but the execute 
+		# commands will take the array of events the athlete is in, loops through that array 
+		# and checks to see if there is a matching record in COMPETEINFO with athleteid and eventid
 		n = 0
 		while n < @objname.eventsarr.size
 			assert_equal DB.execute("SELECT ATHLETEID FROM COMPETEINFO WHERE EVENTID = (SELECT EVENTABV FROM EVENTS WHERE EVENTNAME = \"#{@objname.eventsarr[n]}\")")[0]['ATHLETEID'], @objname.id
@@ -40,7 +53,59 @@ class EntryformSQLTest < Minitest::Test
 
 	end
 
+	def test_found_obj_by_id
+		# checks if object made by using .find_by_id matches the instance information
+
+		assert_equal EntryformSQL.find_by_id(@objname.id)[0]['ATHLETEID'], @objname.id
+		assert_equal EntryformSQL.find_by_id(@objname.id)[0]['NAME'], @objname.name
+		assert_equal EntryformSQL.find_by_id(@objname.id)[0]['COLLEGE'], @objname.college
+		assert_equal EntryformSQL.find_by_id(@objname.id)[0]['ADDRESS'], @objname.address
+		assert_equal EntryformSQL.find_by_id(@objname2.id)[0]['ATHLETEID'], @objname2.id
+
+	end
+
+	def test_found_obj_by_name
+		# checks if .find_by_name works the same way 
+
+		assert_equal EntryformSQL.find_by_name(@objname.name)[0]['NAME'], @objname.name
+		assert_equal EntryformSQL.find_by_name(@objname2.name)[0]['NAME'], @objname2.name
+
+	end
+	
+	def test_update_obj_name_and_college
+		# tests to see if initial info is changed (or not) after updating athlete record
+
+		origname = @objname.name
+		origid = @objname.id
+		origcollege = @objname.college
+		origaddr = @objname.address
+
+		EntryformSQL.update_record_by_id(1, "Billy Bob", "University of Pheonix", "Pheonix, AZ", "100 Freestyle, 200 Freestyle, 400 Intermediate Medley")
+
+		refute_equal origname, EntryformSQL.find_by_id(origid)[0]['NAME']
+		refute_equal origcollege, EntryformSQL.find_by_id(origid)[0]['COLLEGE']
+		assert_equal origid, EntryformSQL.find_by_id(origid)[0]['ATHLETEID']
+		refute_equal @objname.address, EntryformSQL.find_by_id(origid)[0]['ADDRESS']
+
+	end
+
+	def test_update_obj_by_name
+		# Similarly, made function to update record by name, because I would not expect our swimmeet 
+		# coordinator to base every edit off of the ID. Maybe they at first have the thought "I need to
+		# update Kenny's record" instead of "I need to update number 1's record".
 		
+		origid = @objname2.id
+		origcollege = @objname2.college
+		origaddr = @objname2.address
+
+		EntryformSQL.update_record_by_name("Kenny Loggins", "Richards University", "Boston, MA", "50 Freestyle, 100 Breaststroke, 100 Butterfly")
+
+		refute_equal EntryformSQL.find_by_id(origid)[0]['COLLEGE'], origcollege
+		assert_equal EntryformSQL.find_by_id(origid)[0]['ATHLETEID'], origid
+		refute_equal EntryformSQL.find_by_id(origid)[0]['ADDRESS'], origaddr
+
+	end
+
 
 
 end
